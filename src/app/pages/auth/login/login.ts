85d9543af67cd8ab129a9d8d53a6967966ddca58
mrlaige <rudiakov.illia@gmail.com>
2025-12-08 18:42:37 +0200

    login error handling

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,4 +1,4 @@
-import {Component, DestroyRef, inject} from '@angular/core';
+import {Component, DestroyRef, inject, signal} from '@angular/core';
 import {Button} from 'primeng/button';
 import {InputText} from 'primeng/inputtext';
 import {Password} from 'primeng/password';
@@ -8,10 +8,12 @@ import {AuthService} from '../../../services/auth/auth-service';
 import {BasePage} from '../../../components/common/base-page/base-page';
 import {LoginRequest} from '../../../modeles/auth/LoginRequest';
 import {ReactiveFormsModule, Validators} from '@angular/forms';
-import {finalize, take, tap} from 'rxjs';
+import {catchError, finalize, of, take, tap} from 'rxjs';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {AccessToken} from '../../../modeles/auth/AccessToken';
 import {Message} from 'primeng/message';
+import {HttpErrorResponse} from '@angular/common/http';
+import {ApiError} from '../../../modeles/ApiError';
 
 @Component({
   selector: 'app-login',
@@ -31,6 +33,8 @@ export class Login extends BasePage {
   private authService = inject(AuthService);
   private destroyRef = inject(DestroyRef);
 
+  public apiError = signal<ApiError | null>(null);
+
   public form = this.fb.group({
     phone: this.fb.control('', [Validators.required]),
     password: this.fb.control('', [Validators.required]),
@@ -38,12 +42,22 @@ export class Login extends BasePage {
   });
 
   public submit() {
+    if (!this.form.valid) {
+      return;
+    }
+
     this.isLoading.set(true);
+    this.apiError.set(null);
+
     this.authService.login(this.form.value as LoginRequest).pipe(
+      catchError((err: HttpErrorResponse) => {
+        const error = err.error as ApiError;
+        this.apiError.set(error);
+        return of(null);
+      }),
       tap(async res => {
         const fa = res as { require2fa: boolean };
         if (fa.require2fa) {
-
 
           return;
         }
